dfb18f64e01c49e224f068bf7115329910d7236e
Belbut <andreferreirabelbut@gmail.com>
2022-11-12 23:22:33 +0000

    Add repeting game possibility

--- main.rb
+++ main.rb
@@ -1,12 +1,8 @@
 require_relative './lib/game'
 
-a= Game.new
-a.play
+loop do
+  Game.new.play
 
-
-# loop do
-#     Game.new.pay
-  
-#     puts 'Wanna play again? [Y/N]'
-#     break if gets.chomp != 'Y'
-#   end
+  puts 'Wanna play again? [Y/N]'
+  break if gets.chomp != 'Y'
+end
